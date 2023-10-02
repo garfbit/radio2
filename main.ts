08a148f0f33b4919501601c0cfdff6508e982b02
garfbit <104676660+garfbit@users.noreply.github.com>
2023-10-02 13:40:01 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,19 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         basic.showNumber(1)
         pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
     } else if (receivedNumber == 2) {
         basic.showNumber(2)
         pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
     } else if (receivedNumber == 3) {
         basic.showNumber(3)
         pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
     } else if (receivedNumber == 4) {
         basic.showNumber(4)
         pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
     }
 })
 radio.setGroup(1)
